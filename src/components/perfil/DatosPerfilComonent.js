1780be7dbabf36c398c3c6fe78d9aa01dc700533
Breiner Barraza <breinerbarraza22@gmail.com>
2021-11-04 14:18:00 -0500

    Datos Perfil

--- src/components/perfil/DatosPerfilComonent.js
+++ src/components/perfil/DatosPerfilComonent.js
@@ -1,13 +1,64 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
 
 export const DatosPerfilComonent = () => {
     return (
-        <div>
-            <h1>hola mundo</h1>
-            <p><b>Nombre: </b></p>
-            <p><b>Apellidos: </b></p>
-            <p><b>Email: </b></p>
-            <p><b>Usuario: </b></p>
-        </div>
+        <>
+            
+            <div className="datos">
+                <div className="datos-personales">
+                <div className="salir">
+                        <button className="btn btn-primary-outline"><i className="fas fa-angle-left" style={{ marginRight: "10px" }}></i><i class="fas fa-home"></i>Inicio</button>
+                        
+                    <h4 className="h4-datos">Datos personales</h4></div>
+                    <FormControl fullWidth  >
+                        <TextField
+                            type="text"
+                            name="nombre"
+                            placeholder="Escribe..."
+                            label="Nombre"
+                            className="form-control"
+                            style={{ marginBottom: "30px" }}
+                            onChange={""}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            type="text"
+                            name="apellidos"
+                            placeholder="Escribe..."
+                            label="Apellidos"
+                            className="form-control"
+                            style={{ marginBottom: "30px" }}
+                            onChange={""}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            type="text"
+                            name="usuario"
+                            placeholder="Escribe..."
+                            label="Usuario"
+                            className="form-control"
+                            style={{ marginBottom: "30px" }}
+                            onChange={""}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </FormControl>
+                    
+                        <div className="actualizar-cambiar">
+                            <button className="btn btn-primary actualizar"><i className="far fa-edit" style={{ marginRight: "10px" }}></i>Actulizar datos</button>
+                            <button className="btn btn-primary cambiar"><i className="fas fa-key" style={{ marginRight: "10px" }}></i>Cambiar contraseña</button>
+                        </div>
+                    
+                </div>
+            </div>
+
+        </>
     )
 }
