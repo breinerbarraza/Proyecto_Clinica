bd8e855907f2a73f9acac36789c4a626ef805db7
Breiner Barraza <breinerbarraza22@gmail.com>
2021-11-04 15:11:11 -0500

    cambios

--- src/components/perfil/PerfilComponent.js
+++ src/components/perfil/PerfilComponent.js
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom';
 import ClinicaBlanco from '../../image/Recursos-Femto/logo.png';
 export const PerfilComponent = () => {
 
-
     const [datos_perfil, setdatos_perfil] = useState({});
-
     useEffect(() => {
-
         const nombres = JSON.parse(localStorage.getItem('nombres'));
         const apellidos = JSON.parse(localStorage.getItem('apellidos'));
         const id_user = JSON.parse(localStorage.getItem('id_user'));
+        const username = JSON.parse(localStorage.getItem('username'));
         const objeto = {
             id_user,
             nombres,
-            apellidos
+            apellidos,
+            username
         }
         setdatos_perfil(objeto);
     }, []);
 
 
-    const cerrarSesion = (token, idUser, nombres, apellidos)=>{
+    const cerrarSesion = (token, idUser, nombres, apellidos,username)=>{
         localStorage.removeItem(token);
         localStorage.removeItem(idUser);
         localStorage.removeItem(nombres);
         localStorage.removeItem(apellidos);
+        localStorage.removeItem(username);
         window.location = "/";
     }
     
@@ -38,7 +38,7 @@ export const PerfilComponent = () => {
                 <img alt="clinica" src={ClinicaBlanco} width="50" height="50"/>
                 <ul className="dropdown-menu dropdown-menu-end">
                     <li><Link to="/datos_perfil"><button className="dropdown-item" type="button">Perfil</button></Link></li>
-                    <li><button onClick={() => cerrarSesion('token', 'id_user', 'nombres', 'apellidos')} className="dropdown-item" type="button">Cerrar Sesiòn</button></li>
+                    <li><button onClick={() => cerrarSesion('token', 'id_user', 'nombres', 'apellidos', 'username')} className="dropdown-item" type="button">Cerrar Sesiòn</button></li>
                     {/*<li><button className="dropdown-item" type="button">Something else here</button></li>*/}
                 </ul>
             </div>
