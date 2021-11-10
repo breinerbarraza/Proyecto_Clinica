2275345d9812fe67b02c7af0eed2e5130dc2f0ff
Omar junior <omarhernandez041204@gmail.com>
2021-11-10 17:05:48 -0500

    Cambio de contraseña

--- src/components/perfil/CambioContraseña.js
+++ src/components/perfil/CambioContraseña.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import { Link } from 'react-router-dom';
+import API from '../../Utils/API';
+
 export const CambioContraseña = () => {
+
+    const [data_password, setPassword] = useState([]);
+    const [data_user, setData_user] = useState([]);
+
+    useEffect(()=>{
+        const id = JSON.parse(localStorage.getItem('id_user'))
+        console.log(id)
+        API.get('api/usuarios/user/'+id)
+        .then( data => {
+            const respuesta = data.data;
+            console.log(respuesta)
+            setData_user(respuesta)
+
+        } )
+    },[]);
+
+    const handleInputChange = (e)=>
+    {
+        setPassword({
+            ...data_password,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleChangePassword = (e)=>{
+        e.preventDefault()
+        console.log("Entro en el cambio de contraseña")
+        data_password.password_user = data_user.password
+        console.log(data_password)
+        API.put('api/usuarios/user/updated_password/', JSON.stringify(data_password))
+        .then( resp =>{
+            const respuesta = resp.data;
+            if(respuesta.msg){
+                alert(respuesta.msg)
+            }else if(respuesta.error){
+                alert(respuesta.error)
+            }else{
+                const msg_affirmation = respuesta.data;
+                alert(msg_affirmation)
+                window.location = "/datos_perfil"
+            }
+        })
+    }
+
     return (
         <div>
             <>
@@ -13,14 +59,15 @@ export const CambioContraseña = () => {
                              <button className="btn btn-primary-outline inicio"><i className="fas fa-angle-left" style={{ marginRight: "10px" }}></i><i className="far fa-address-card"></i> Datos personales</button>
                         </Link>
                     <h4 className="h4-datos">Cambiar contraseña</h4></div>
-                    <form>
+                    <form onSubmit={handleChangePassword}>
                     <FormControl fullWidth >
                         <TextField
                             type="password"
-                            name="contraseña"
+                            name="old_password"
                             placeholder="Escribe..."
                             label="Contraseña antigua"
                             className="form-control"
+                            onChange={handleInputChange}
                             required
                             style={{ marginBottom: "30px" }}
                             InputLabelProps={{
@@ -29,10 +76,11 @@ export const CambioContraseña = () => {
                         />
                         <TextField
                             type="password"
-                            name="contraseña1"
+                            name="password"
                             placeholder="Escribe..."
                             label="Nueva contraseña"
                             className="form-control"
+                            onChange={handleInputChange}
                             required
                             style={{ marginBottom: "30px" }}
                             InputLabelProps={{  
@@ -41,10 +89,11 @@ export const CambioContraseña = () => {
                         />
                         <TextField
                             type="password"
-                            name="contraseña2"
+                            name="password2"
                             placeholder="Escribe..."
                             label="Confirmar contraseña"
                             className="form-control"
+                            onChange={handleInputChange}
                             required
                             style={{ marginBottom: "30px" }}
                             InputLabelProps={{
@@ -52,13 +101,10 @@ export const CambioContraseña = () => {
                             }}
                         />
                     </FormControl>
-
                         <div className="actualizar-cambiar">
-                            <Link to="/datos_perfil">
-                                <button className="btn btn-primary actualizar"><i className="far fa-edit" style={{ marginRight: "10px" }}></i>Actulizar contraseña</button>
-                            </Link>
+                            <button className="btn btn-primary actualizar2" type="submit"><i className="far fa-edit" style={{ marginRight: "10px" }}></i>Actualizar contraseña</button>
                         </div>
-                        </form>
+                    </form>
                 </div>
             </div>
         </>
