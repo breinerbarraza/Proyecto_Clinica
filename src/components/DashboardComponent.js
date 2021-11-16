88bb6fcfbb9ba82337ff2fe1b922d7c2d0bd24f7
Breiner Barraza <breinerbarraza22@gmail.com>
2021-11-16 11:03:09 -0500

    update

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -22,7 +22,7 @@ export const DashboardComponent = () => {
     const load = async () => {
         await API('/api/referidos/')
             .then(response => {
-                console.log(response.data)
+                // console.log(response.data)
                 let agrupacion = _.chain(response.data).groupBy('estadoReferido')
                     .map((value, key) => ({
                         "estado": key,
@@ -30,17 +30,21 @@ export const DashboardComponent = () => {
                         "color": 'rgba(' + (Math.floor(Math.random() * 256)) + ','
                             + (Math.floor(Math.random() * 256)) + ','
                             + (Math.floor(Math.random() * 255)) + ', 0.8)'
+                            
                     }))
                 let agrupacionArray = _.toArray(agrupacion)
+                console.log(agrupacionArray)
                 setPieChartData(agrupacionArray)
-                console.log(pieChartData)
-                pieChartData.map((el) => (
+                console.log(agrupacionArray)
+                agrupacionArray.map((el) => (
                     setLabelColors(labelColors => [...labelColors, el.color]),
                     setTiposFormulario(tiposFormulario => [...tiposFormulario, el.estado]),
-                    setCantidades(cantidades => [...cantidades, el.valor])
-                    //console.log(`Estados: ${tiposFormulario}`)
+                    setCantidades(cantidades => [...cantidades, el.valor]),
+                    console.log(`Estados: ${el}`),
+                console.log("el valor"+labelColors)
                 ))
             }).catch(console.error)
+
         setInfoFull({
             labels: tiposFormulario,
             datasets: [{
@@ -51,12 +55,11 @@ export const DashboardComponent = () => {
             }]
         })
     }
-    // console.log(pieChartData)
 
     useEffect(() => {
         load()
-    }, [])
 
+    }, [])
     return (
         <>
             <HeaderComponent dashboard />
@@ -92,11 +95,12 @@ export const DashboardComponent = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {pieChartData.map((el, key) => (
+                                    {pieChartData.map((dato, key) => (
                                         <tr key={key}>
-                                            <td>{el.estado}</td>
-                                            <td>{el.valor}</td>
+                                            <td>{dato.estado}</td>
+                                            <td>{dato.valor}</td>
                                         </tr>
+                                        
                                     ))}
                                 </tbody>
                             </table>
