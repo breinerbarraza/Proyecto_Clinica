a9e57f9495804d927eeef09ff0c6fc63e99032e7
User <user@MacBook-Pro-de-User-2.local>
2021-12-06 15:29:49 -0500

    cambio de ip a server

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const getToken = () => `Token ${localStorage.getItem('token')}`;
 
 const API = axios.create({
-  baseURL: "http://51.222.13.17/",
+  baseURL: "http://51.222.13.17:8081/",
   headers: {
     'Content-Type': 'application/json'
   }
