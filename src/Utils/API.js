4a3a85f1b22c050031234daff7ca622fbae0b50e
User <user@MacBook-Pro-de-User-2.local>
2021-12-06 15:08:49 -0500

    Arreglo de la ip

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const getToken = () => `Token ${localStorage.getItem('token')}`;
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "http://51.222.13.17/",
   headers: {
     'Content-Type': 'application/json'
   }
