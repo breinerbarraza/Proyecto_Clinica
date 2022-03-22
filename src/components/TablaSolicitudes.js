7a1d012b89cd94e3c7944121acf61f50964ba14e
Omar junior <omarhernandez041204@gmail.com>
2022-03-22 17:01:52 -0500

    tabla solicitud filtros

--- src/components/TablaSolicitudes.js
+++ src/components/TablaSolicitudes.js
@@ -4,10 +4,28 @@ import { MDBDataTable } from 'mdbreact'
 import { PerfilComponent } from './perfil/PerfilComponent'
 import { HeaderComponent } from './HeaderComponent'
 import API from '../Utils/API'
+import { InputGroup } from 'reactstrap'
+var moment = require('moment')
 
 export const TablaSolicitudes = () => {
     
     const [solicitudes, setSolicitudes] = useState([])
+    const [fechaInicial, setFechaInicial] = useState("")
+    const [fechaFinal, setFechaFinal] = useState("")
+    const [today, setToday] = useState(new Date())
+    
+    useEffect(()=> {
+        let super_user = (JSON.parse(localStorage.getItem("super_user"))) ? JSON.parse(localStorage.getItem("super_user")) : "";
+        let id_empleado = (JSON.parse(localStorage.getItem("id_user"))) ? JSON.parse(localStorage.getItem("id_user")) : ""
+        if(super_user){
+            //Solicitudes pendientes generales que le aparecera al admministrador
+            getCambioEstadoGeneral()
+        }else{
+            //Solicitudes pendientes que le aparecera al empleado
+            getCambioEstadoById(id_empleado)
+        }
+        
+    }, []);
 
     const getCambioEstadoGeneral = ()=>{
         API.get('api/referidos_cambio_estado/?recordatorio=2')
@@ -30,6 +48,29 @@ export const TablaSolicitudes = () => {
 
     const getCambioEstadoById = (id)=>{
         API.get(`api/referidos_cambio_estado/?id_empleado=${id}`)
+        .then( ({data}) => {
+            const resp = data
+            resp.map((item) =>{
+                setSolicitudes(solicitudes => [...solicitudes,{  
+                    "asesor": item.empleadoInicial,
+                    "referido": item.referido,
+                    "correo_electronico": (item.correo_electronico) ? item.correo_electronico : "-",
+                    "estadoNuevo": item.estadoNuevo,
+                    "fecha": item.fecha,
+                    "observaciones"  : item.observaciones
+                }])
+                
+            })
+        })
+        .catch(console.error)
+    }
+
+    const filtrarDatos = (e)=>{ 
+        if(fechaFinal == "" || fechaInicial == ""){
+            return alert("Digite un rango de fechas")
+        }
+        setSolicitudes([])
+        API.get(`api/referidos_cambio_estado/?fechaInicio=${fechaInicial}&fechaFinal=${fechaFinal}`)
         .then( ({data}) => {
             const resp = data
             resp.map((item) =>{
@@ -110,25 +151,53 @@ export const TablaSolicitudes = () => {
         )
     }
 
-    useEffect(()=> {
-        let super_user = (JSON.parse(localStorage.getItem("super_user"))) ? JSON.parse(localStorage.getItem("super_user")) : "";
-        let id_empleado = (JSON.parse(localStorage.getItem("id_user"))) ? JSON.parse(localStorage.getItem("id_user")) : ""
-        if(super_user){
-            //Solicitudes pendientes generales que le aparecera al admministrador
-            getCambioEstadoGeneral()
-        }else{
-            //Solicitudes pendientes que le aparecera al empleado
-            getCambioEstadoById(id_empleado)
-        }
-        
-    }, []);
-
   return (
     <>
       <PerfilComponent />
       <HeaderComponent users={false} dashboard={true} />
       <div class="lista-container">
-        <h2 style={{marginBottom: "26px", fontSize: '28px' , color: "#02305b"}}>Solicitudes pendientes</h2>
+        <h2 style={{fontSize: '25px', color: "#02305b"}}>Solicitudes pendientes</h2>
+            <div style={{
+                 display: 'inline-block', width: 600, padding: 10
+            }}>
+                <InputGroup>
+                    <input
+                        // InputProps={{inputProps: { min: moment(today).format('YYYY-MM-DD')} }}
+                        min={moment(today).format('YYYY-MM-DD')}
+                        style={{padding:15}}
+                        type="date"
+                        className="form-control"
+                        name="fecha-inicio"
+                        value={fechaInicial}
+                        placeholder="Fecha inicial"
+                        onChange={(e) => setFechaInicial(e.target.value)}
+                        required
+                    />
+                    <input
+                        min={moment(today).format('YYYY-MM-DD')}
+                        style={{padding:15}}
+                        type="date"
+                        className="form-control"
+                        name="fecha-final"
+                        value={fechaFinal}
+                        placeholder="Fecha final"
+                        onChange={(e) => setFechaFinal(e.target.value)}
+                        required
+                    />
+                </InputGroup>
+
+            </div>
+            <div style={{display:'inline-block'}}>
+                <button style={{ 
+                    backgroundColor: '#6b8dd6', 
+                    color:'white',
+                    marginLeft:'5px',
+                    border:'none',
+                    padding:4,
+                    letterSpacing:1
+                    }} 
+                    onClick={()=> { filtrarDatos() }}>Filtrar</button>
+            </div>
             <div className="tabla-lista">
                 {showTable()}
             </div>   
