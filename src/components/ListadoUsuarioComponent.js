16a7145c670a57dc7bcc1b6383fbec0d1050d761
Omar junior <omarhernandez041204@gmail.com>
2022-03-30 10:19:03 -0500

    cambios en el listado de usuarios admin

--- src/components/ListadoUsuarioComponent.js
+++ src/components/ListadoUsuarioComponent.js
@@ -43,8 +43,8 @@ export const ListadoUsuarioComponent = () => {
             "numeroIdentificacion": (item.numeroIdentificacion) ? item.numeroIdentificacion : "Aun no cuenta con identificacion",
             "correo_electronico": item.email,
             "referidos": (item.total_referidos) ? item.total_referidos : 0,
-            "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
-            "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
+            "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
+            "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
             "rol": item.rol_,
             "is_active": <input onChange={item.is_active ? (e) => handleChangeActivo(e, item.id) : (e) => handleChangeNoActivo(e, item.id)} type="checkbox" checked={item.is_active && true} />
           }])
@@ -174,8 +174,8 @@ export const ListadoUsuarioComponent = () => {
                   "numeroIdentificacion": (item.numeroIdentificacion) ? item.numeroIdentificacion : "Aun no cuenta con identificacion",
                   "correo_electronico": item.email,
                   "referidos": (item.total_referidos) ? item.total_referidos : 0,
-                  "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
-                  "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
+                  "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
+                  "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
                   "rol": item.rol_,
                   "is_active": <input onChange={item.is_active ? (e) => handleChangeActivo(e, item.id) : (e) => handleChangeNoActivo(e, item.id)} type="checkbox" checked={item.is_active && true} />
                 }])
@@ -198,8 +198,8 @@ export const ListadoUsuarioComponent = () => {
               "numeroIdentificacion": (item.numeroIdentificacion) ? item.numeroIdentificacion : "Aun no cuenta con identificacion",
               "correo_electronico": item.email,
               "referidos": (item.total_referidos) ? item.total_referidos : 0,
-              "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
-              "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
+              "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
+              "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
               "rol": item.rol_,
               "is_active": <input onChange={item.is_active ? (e) => handleChangeActivo(e, item.id) : (e) => handleChangeNoActivo(e, item.id)} type="checkbox" checked={item.is_active && true} />
             }])
@@ -224,8 +224,8 @@ export const ListadoUsuarioComponent = () => {
           "numeroIdentificacion": (item.numeroIdentificacion) ? item.numeroIdentificacion : "Aun no cuenta con identificacion",
           "correo_electronico": item.email,
           "referidos": (item.total_referidos) ? item.total_referidos : 0,
-          "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
-          "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`http://51.222.13.17:8081/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
+          "QR_Paciente": (item.codigoqr_referidos == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_referidos}.png`}><span title="QR Paciente"><i className="fas fa-qrcode" ></i></span></a>,
+          "QR_Asesor": (item.codigoqr_asesor == "") ? "" : <a href={`https://app.femto.live/media/uploads/${item.codigoqr_asesor}.png`}><span title="QR Asesor"><i className="fas fa-qrcode" ></i></span></a>,
           "rol": item.rol_,
           "is_active": <input onChange={item.is_active ? (e) => handleChangeActivo(e, item.id) : (e) => handleChangeNoActivo(e, item.id)} type="checkbox" checked={item.is_active && true} />
         }])
